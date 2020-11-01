350115e4888d3b0cbe40effb887fb4c94acda0f5
Fujinodaiki <523.snow@gmail.com>
2020-11-01 18:03:35 +0900

    コミュニティ新規作成画面を作成。新規作成ができるようにモデルとテーブルの作成を行った

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true, length: { maximum: 6 }
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
   devise_for :users
-  get 'messages/index'
+  resources :messages, only: [:index,]
   root to: "messages#index"
+  resources :rooms, only: [:new, :create]
 end
